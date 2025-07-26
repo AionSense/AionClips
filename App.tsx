4e1c3b075879cb1f5f2041fd8ed511ad699cc829
AionSense <bryson@aionsense.com>
2025-07-26 01:52:58 -0600

    Add TikTok verification route at /terms/ path

--- App.tsx
+++ App.tsx
@@ -8,6 +8,15 @@ import { ComingSoon } from './components/ComingSoon';
 import { LiquidCursor } from './components/LiquidCursor';
 import { BackButton } from './components/BackButton';
 
+// TikTok verification component
+function TikTokVerification() {
+  return (
+    <div style={{ fontFamily: 'monospace', padding: '20px' }}>
+      tiktok-developers-site-verification=AETqyH7LkoxihYCUU2qqhphJXxEe2Oc6
+    </div>
+  );
+}
+
 function AppContent() {
   const location = useLocation();
   const isLandingPage = location.pathname === '/';
@@ -20,6 +29,7 @@ function AppContent() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/terms/tiktokAETqyH7LkoxihYCUU2qqhphJXxEe2Oc6.txt" element={<TikTokVerification />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/coming-soon" element={<ComingSoon />} />
       </Routes>
